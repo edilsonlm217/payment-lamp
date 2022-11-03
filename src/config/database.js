b70c902a936a2953c6257d290082b30de5e4f691
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-03 13:31:28 -0400

    Implementar arquivo de configuração do database com variaveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ const { MONGO_DB_AUTH_SOURCE, MONGO_DB_USER, MONGO_DB_PW, MONGO_DB_NAME } = proc
 const mongoUrl = `mongodb://${MONGO_DB_USER}:${MONGO_DB_PW}@localhost:27017/${MONGO_DB_NAME}?authSource=${MONGO_DB_AUTH_SOURCE}&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass&ssl=false`;
 
 export default {
-    mongodb_auth_source: 'admin',
+    mongodb_auth_source: MONGO_DB_AUTH_SOURCE,
     mongodb_url: mongoUrl,
-    mongodb_user: 'root',
-    mongodb_password: 'Falcon2931',
+    mongodb_user: MONGO_DB_USER,
+    mongodb_password: MONGO_DB_PW,
 };
