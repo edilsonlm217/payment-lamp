645726916c212fa2d83f08355865e24be5fa71d0
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-07 18:49:34 -0400

    Criar rota session

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import PlanController from './app/controllers/PlanController';
+import SessionController from './app/controllers/SessionController';
 
 import RouteParamValidator from './app/middlewares/RouteParamsValidator';
 
 const routes = new Router();
 
 routes.use(RouteParamValidator);
-routes.get('/plans', PlanController.index)
-routes.post('/plans', PlanController.store)
+
+routes.get('/plans', PlanController.index);
+routes.post('/plans', PlanController.store);
+
+routes.post('/session', SessionController.store);
 
 
 export default routes;
