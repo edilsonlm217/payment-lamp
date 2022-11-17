47adb4eb0329a61402f9bca6803d1e0b702c1b9a
Edilson Lima <edilson.lima.3@globalhitss.com.br>
2022-11-16 23:37:46 -0400

    Refatoração do middleware de validação dos parametros de rota

--- src/app/middlewares/RouteParamsValidator.js
+++ src/app/middlewares/RouteParamsValidator.js
@@ -1,57 +1,43 @@
-import { object, string, number } from 'yup';
+import CreatePlan from '../validators/CreatePlan';
+import GenCardToken from '../validators/GenCardToken';
+import SubscribePlan from '../validators/SubscribePlan';
+
+const routesToValidate = [
+  {
+    url: "/plans",
+    method: "POST",
+    validator: CreatePlan
+  },
+  {
+    url: "/generate-token",
+    method: "POST",
+    validator: GenCardToken
+  },
+  {
+    url: "/subscription",
+    method: "POST",
+    validator: SubscribePlan
+  },
+];
 
 export default async function (req, res, next) {
-    if (req.url === "/plans" && req.method === "POST") {
-        try {
-            await CreatePlanParams.validate(req.body, { strict: true });
-            next();
-            return;
-        } catch (error) {
-            const { message } = error;
-            return res.status(400).json({
-                title: "Invalid params",
-                detail: message,
-                status: 400,
-            });
-        }
-    }
+  const match = routesToValidate.filter(route =>
+    route.url === req.url &&
+    route.method === req.method
+  )[0];
 
+  if (!match) { next() }
 
-    if (req.url === "/generate-token" && req.method === "POST") {
-        try {
-            await GenCardTokenParams.validate(req.body, { strict: true });
-            next();
-            return;
-        } catch (error) {
-            const { message } = error;
-            return res.status(400).json({
-                title: "Invalid params",
-                detail: message,
-                status: 400,
-            });
-        }
-    }
-
+  try {
+    const { validator } = match;
+    await validator.validate(req.body, { strict: true });
     next();
+  } catch (error) {
+    const { message } = error;
+    return res.status(400).json({
+      title: "Invalid params",
+      detail: message,
+      status: 400,
+    });
+  }
 }
-
-const GenCardTokenParams = object({
-    sessionId: string().required(),
-    amount: string().required(),
-    cardNumber: string().required(),
-    cardBrand: string().required(),
-    cardCvv: string().required(),
-    cardExpirationMonth: string().required(),
-    cardExpirationYear: string().required(),
-});
-
-const CreatePlanParams = object({
-    name: string().required(),
-    period: string().required(),
-    amountPerPayment: number().required().positive().test(
-        'is-decimal',
-        'Invalid decimal on amountPerPayment',
-        value => (value + "").match(/^\d*\.{1}\d*$/)),
-    maxUses: number().required().positive(),
-    description: string().required(),
-});
